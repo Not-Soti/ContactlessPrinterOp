e005181d254ef604f4493132115728f8f640286d
uo258375 <uo258375@uniovi.es>
2020-12-08 13:48:05 +0100

    La aplicación ya imprime

--- movil/src/main/java/com/example/movil/MainActivity.kt
+++ movil/src/main/java/com/example/movil/MainActivity.kt
@@ -1,38 +1,30 @@
 package com.example.movil
 
-import android.Manifest
-import android.content.Context
-import android.content.DialogInterface
+
 import android.content.Intent
-import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Vibrator
 import android.util.Log
-import android.util.SparseArray
-import android.view.SurfaceHolder
-import android.view.SurfaceView
+
 import android.view.View
 import android.widget.Button
-import android.widget.TextView
-import androidx.appcompat.app.AlertDialog
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
-import com.google.android.gms.vision.CameraSource
-import com.google.android.gms.vision.Detector
-import com.google.android.gms.vision.barcode.Barcode
-import com.google.android.gms.vision.barcode.BarcodeDetector
-import java.lang.Exception
+
+import androidx.print.PrintHelper
+
 
 class MainActivity : AppCompatActivity(){
 
     lateinit var buttonReadQr: Button
+    lateinit var buttonPrint: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         buttonReadQr = findViewById(R.id.act_main_readQrButton)
+        buttonPrint = findViewById(R.id.act_main_printButton)
 
         //Al pulsar este boton se va a la actividad que lee los QR
         buttonReadQr.setOnClickListener(object : View.OnClickListener{
@@ -40,6 +32,18 @@ class MainActivity : AppCompatActivity(){
                 val i = Intent(this@MainActivity, ReadQrActivity::class.java)
                 startActivity(i)
             }
+        })
+
+        //Print button listener
+        buttonPrint.setOnClickListener(object: View.OnClickListener{
+            override fun onClick(p0: View?) {
+                var printHelper: PrintHelper = PrintHelper(this@MainActivity)
+                printHelper.scaleMode = PrintHelper.SCALE_MODE_FIT
+                var photo: Bitmap = BitmapFactory.decodeResource(resources, R.mipmap.foto_prueba)
+                printHelper.printBitmap("App movil", photo)
+                Log.d("---IMPRIMIR---", "Impreso")
+            }
+
         })
     }
 

839664769f69af19ef364439c4fd63ec93ebbbb4
uo258375 <uo258375@uniovi.es>
2020-12-28 13:36:07 +0100

    Ahora imprime (pero no previsualiza) HTML

--- movil/src/main/java/com/example/movil/PrintActivity.kt
+++ movil/src/main/java/com/example/movil/PrintActivity.kt
@@ -7,6 +7,7 @@ import android.content.Intent
 import android.graphics.Bitmap
 import android.graphics.pdf.PdfRenderer
 import android.net.Uri
+import android.os.AsyncTask
 import android.os.Bundle
 import android.os.ParcelFileDescriptor
 import android.print.PrintAttributes
@@ -14,11 +15,18 @@ import android.print.PrintManager
 import android.provider.MediaStore
 import android.util.Log
 import android.view.View
+import android.view.ViewGroup
+import android.webkit.WebResourceRequest
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat.getSystemService
 import androidx.print.PrintHelper
+import com.izettle.html2bitmap.Html2Bitmap
+import com.izettle.html2bitmap.content.WebViewContent
 import java.io.File
 
 
@@ -31,14 +39,15 @@ class PrintActivity : AppCompatActivity() {
     lateinit var buttonSendEmail: Button
     lateinit var buttonPrint: Button
     lateinit var imagePreview: ImageView
+    lateinit var webPreview: WebView
 
 
     //Request codes for each activity with a result
     val chooseImageRequestCode = 0
     val chooseFileRequestCode = 1
 
-    //var imageUri: Uri? = null //selected image uri
-    var resourceUri: Uri? = null //selected image uri
+
+    var resourceUri: Uri? = null //selected resource uri
     var resourcePath : String? = null //using the uri does not work when trying to print documents
     var resourceType = ResourceTypeEnum.NOT_DEFINED
 
@@ -49,6 +58,7 @@ class PrintActivity : AppCompatActivity() {
         NOT_DEFINED,
         IMAGE,
         PDF,
+        HTML,
         DOCUMENT
     }
 
@@ -62,6 +72,7 @@ class PrintActivity : AppCompatActivity() {
         buttonSendEmail = findViewById(R.id.act_print_sendEmailButton)
         buttonPrint = findViewById(R.id.act_print_printButton)
         imagePreview = findViewById(R.id.act_print_imagePreview)
+        webPreview = findViewById(R.id.act_print_webPreview)
 
         //ChooseFile button listener
         buttonChooseFile.setOnClickListener(object : View.OnClickListener {
@@ -84,25 +95,29 @@ class PrintActivity : AppCompatActivity() {
         })
 
         //SendEmail button listener
-        buttonSendEmail.setOnClickListener(object : View.OnClickListener{
+        buttonSendEmail.setOnClickListener(object : View.OnClickListener {
             override fun onClick(p0: View?) {
 
                 //Nothing was selected
-                if(resourceType == ResourceTypeEnum.NOT_DEFINED){
-                    Toast.makeText(this@PrintActivity, "Selecciona algún archivo", Toast.LENGTH_LONG)
-                }else{
+                if (resourceType == ResourceTypeEnum.NOT_DEFINED) {
+                    Toast.makeText(
+                        this@PrintActivity,
+                        "Selecciona algún archivo",
+                        Toast.LENGTH_LONG
+                    ).show()
+                } else {
 
                     //Open email app and load info
                     val emailIntent = Intent(Intent.ACTION_SEND)
                     //emailIntent.type = "message/rfc822" //Shows only email clients
-                    emailIntent.type="*/*"
+                    emailIntent.type = "*/*"
 
-                    val to : Array<String> = emptyArray()
+                    val to: Array<String> = emptyArray()
                     emailIntent.putExtra(Intent.EXTRA_EMAIL, to)
 
-                    if(resourceType == ResourceTypeEnum.IMAGE){
+                    if (resourceType == ResourceTypeEnum.IMAGE) {
                         emailIntent.putExtra(Intent.EXTRA_STREAM, resourceUri)
-                    }else if(resourceType == ResourceTypeEnum.PDF){
+                    } else if (resourceType == ResourceTypeEnum.PDF) {
                         emailIntent.putExtra(Intent.EXTRA_STREAM, resourceUri)
                     }
 
@@ -120,7 +135,7 @@ class PrintActivity : AppCompatActivity() {
                 //Check for the resource type (photo, doc...)
                 when (resourceType) {
 
-                    //Printing a image
+                    //Printing an image
                     ResourceTypeEnum.IMAGE -> {
                         var printHelper: PrintHelper = PrintHelper(this@PrintActivity)
                         printHelper.scaleMode = PrintHelper.SCALE_MODE_FILL
@@ -139,6 +154,25 @@ class PrintActivity : AppCompatActivity() {
                         val printAdapter = PdfDocumentAdapter(resourcePath!!)
                         printManager.print("PDF", printAdapter, PrintAttributes.Builder().build())
                     }
+
+                    //Printing an HTML
+                    ResourceTypeEnum.HTML -> {
+                        //Creating a webView for printing
+                        val webView = WebView(this@PrintActivity)
+                        webView.webViewClient = object : WebViewClient() {
+
+                            override fun shouldOverrideUrlLoading(view: WebView, request: WebResourceRequest) = false
+
+                            override fun onPageFinished(view: WebView, url: String) {
+                                Log.i(tag, "page finished loading $url")
+                                createWebPrintJob(view)
+                                //mWebView = null
+                            }
+                        }
+
+                        webView.loadUrl(resourceUri.toString()!!)
+
+                    }
                 }
             }
         })
@@ -150,18 +184,9 @@ class PrintActivity : AppCompatActivity() {
 
         //Remove a image from the imagePreview if there was any
         imagePreview.setImageDrawable(null)
+        webPreview.loadUrl("about:blank")
 
         when (requestCode) {
-            //Image selected
-                /*
-            chooseImageRequestCode -> if (data != null) {
-                resourceType = ResourceTypeEnum.IMAGE
-                imageUri = data.data
-                Log.d(tag, "image uri $imageUri")
-                imagePreview.setImageURI(imageUri)
-            }
-            */
-
             //Document selected
             chooseFileRequestCode -> if (data != null) {
                 resourceUri = data.data
@@ -175,22 +200,35 @@ class PrintActivity : AppCompatActivity() {
 
                 val file = File(resourcePath)
                 val extension = file.extension
-                Log.d(tag,"extension  $extension")
+                Log.d(tag, "extension  $extension")
 
                 //Continue depending on the file extension
-                when(extension.toLowerCase()){
-                    "jpg","jpeg", "jpe", "png","bmp", "gif", "webp" -> {
+                when (extension.toLowerCase()) {
+                    "jpg", "jpeg", "jpe", "png", "bmp", "gif", "webp" -> {
                         resourceType = ResourceTypeEnum.IMAGE
                         Log.d(tag, "image uri $resourceUri")
+
+                        //Make the image preview bigger
+                        imagePreview.layoutParams.height = ViewGroup.LayoutParams.MATCH_PARENT
                         imagePreview.setImageURI(resourceUri)
                     }
                     "pdf" -> {
                         resourceType = ResourceTypeEnum.PDF
+                        //Make the image preview bigger
+                        imagePreview.layoutParams.height = ViewGroup.LayoutParams.MATCH_PARENT
                         previewPdf(file)
+                    }
+                    "html" ->{
+                        resourceType = ResourceTypeEnum.HTML
+                        Toast.makeText(this@PrintActivity,"Seleccionado HTML", Toast.LENGTH_LONG).show()
                     }
                     else -> {
                         Log.d(tag, "Extension no soportada")
-                        Toast.makeText(this@PrintActivity, "Extension no soportada", Toast.LENGTH_LONG)//TODO crear dialogo de extensiones soportada
+                        Toast.makeText(
+                            this@PrintActivity,
+                            "Extension no soportada",
+                            Toast.LENGTH_LONG
+                        ).show()//TODO crear dialogo de extensiones soportada
                     }
                 }
             }
@@ -200,7 +238,7 @@ class PrintActivity : AppCompatActivity() {
     /**
      * Function to operate when a pdf is selected
      */
-    private fun previewPdf(file : File){
+    private fun previewPdf(file: File){
 
         /* Render the first page of the document */
 
@@ -233,10 +271,24 @@ class PrintActivity : AppCompatActivity() {
 
     }
 
+    private fun createWebPrintJob(webView: WebView){
+        //Create the print job
+        // Get a PrintManager instance
+        val printManager: PrintManager = this@PrintActivity.getSystemService(
+            Context.PRINT_SERVICE
+        ) as PrintManager
+
+        val printAdapter = webView.createPrintDocumentAdapter("${getString(R.string.app_name)} HTML")
+        printManager.print("Documento HTML", printAdapter, PrintAttributes.Builder().build())
+
+    }
+
     //On back pressed go to main activity
     override fun onBackPressed() {
         super.onBackPressed()
         startActivity(Intent(this, MainActivity::class.java))
     }
+
+
 
 }
